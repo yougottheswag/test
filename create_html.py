6d9954ea5244aebc25933658cd8139ffc4b95607
Helen Jiang <helenjia@compute-3-29.local>
2019-09-29 03:37:55 +0000

    slightly updated

---
+++ create_html.py
@@ -0,0 +1,31 @@
+from html4vision import Col, imagetable
+import os
+
+# table description
+
+#curr_dir = '/Users/helenjiang/Downloads/HTML4Vision/html4vision_code/'
+
+#loop through vis_gt to get the img ids
+#sort the img ids
+
+tp_fp_images = []
+for tp_fp_image in sorted(os.listdir('full/vis_gt/')):
+	tp_fp_image_path = os.path.join('full/vis_gt', tp_fp_image)
+	tp_fp_images.append(tp_fp_image_path)
+
+detection_images = []
+for detection_image in sorted(os.listdir('full/vis_detect/')):
+	detection_image_path = os.path.join('full/vis_detect', detection_image)
+	detection_images.append(detection_image_path)
+
+print(tp_fp_images[0])
+print(detection_images[0])
+cols = [
+  # Col('id1', 'ID'),                                               # make a column of 1-based indice
+  # Col('img', 'Images with TP/FP', 'full/vis_gt/*.png'),             # specify image content for column 2
+  Col('img', 'Images with TP/FP', tp_fp_images),
+  Col('img', 'Images with Detections', detection_images),     # specify image content for column 3
+]
+
+# html table generation
+imagetable(cols)
